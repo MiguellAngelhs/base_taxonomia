794c7e17e289486d3606462814e976b79b26c82c
alexander.moreno <alexander.moreno@delphianalytics.co>
2022-02-24 08:42:10 -0500

    In this commit I did a change in the conect to database.

--- extraccion/controllers/search.py
+++ extraccion/controllers/search.py
@@ -1,12 +1,30 @@
 from extraccion.models import  Taxonomia
 import mysql.connector  as sql
+import os
+import site
+from dotenv import load_dotenv
+load_dotenv()
+Usuerdb = os.getenv('DB_USER')
+site.addsitedir(Usuerdb)
+passworddb = os.getenv('DB_PASSWD')
+site.addsitedir(passworddb)
+basedb = os.getenv('DB_NAME')
+site.addsitedir(basedb)
+sqlhost = os.getenv('HOST')
+site.addsitedir(sqlhost)
 
+
+'''Capa de negocios'''
+
+'''conexión de la database para el buscador  de palabras'''
 db =sql.connect(
-            host = "localhost",
-            user = "sammy",
-            passwd ="password",
-            database="pb_taxonomia"
+            host = sqlhost,
+            user = Usuerdb,
+            passwd =passworddb,
+            database=basedb
         )
+
+
 
 def search(q):
     q=str(q)
